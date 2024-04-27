aa7d676f67c36dbda4f3ce2b528bf076e4c37e47
Kayode <kayodefredrickbalogun@gmail.com>
2024-04-28 00:17:40 +0100

    add api key to netlify

--- src/components/AttachOptions.tsx
+++ src/components/AttachOptions.tsx
@@ -19,9 +19,8 @@ const AttachOptions = () => {
 
   const handleGoogleDriveUpload = () => {
     openPicker({
-      clientId:
-        "944794798911-209eeb45av65hort0fpjtod478t1dpub.apps.googleusercontent.com",
-      developerKey: "AIzaSyAnWHIR5y6hdnzJLLAwDuW-rBGV5gIGFqw",
+      clientId: `${process.env.GOOGLE_CLIENT_ID}`,
+      developerKey: `key=${process.env.API_KEY}`,
       viewId: "DOCS",
       // token: token, // pass oauth token in case you already have one
       showUploadView: true,
