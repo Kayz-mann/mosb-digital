6253c37361bfb676e2d64c69bf10b2d3413607f2
Kayode <kayodefredrickbalogun@gmail.com>
2024-05-02 14:37:29 +0100

    fix deployment error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
-"use client";
 import AboutContent from "@/components/AboutContent";
 import Content from "@/components/Content";
 import Header from "@/components/Header";
 import ImageAdContent from "@/components/ImageAdContent";
 import ServicesContent from "@/components/ServicesContent";
 import SponsorContent from "@/components/SponsorContent";
-import useMobileOrTablet from "./hooks/useMobileOrTablet";
 import SliderContent from "@/components/SliderContent";
 import Footer from "@/components/Footer";
 
 export default function Home() {
-  useMobileOrTablet();
-
   return (
     <main>
       <Header />
