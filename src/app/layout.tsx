e84b5d5d0d398d1a56193f03b3c39351b73e25ba
Kayode <kayodefredrickbalogun@gmail.com>
2024-05-28 14:08:37 +0100

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import { ReactQueryClientProvider } from "./ReactQueryClientProvider";
 import { Toaster } from "react-hot-toast";
-import { useMetadata } from "./hooks/useMetaData";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,8 +10,10 @@ export const metadata = {
   description:
     "Last year, investors were hopeful that 2024 would see a lot of companies going public through IPOs (Initial Public Offerings). However, as we near the middle of 2024, those hopes have mostly faded.",
   openGraph: {
-    title: "Default Title",
-    description: "Default Description",
+    title:
+      "From Plaid to Figma, here’s why many tech startups are postponing their IPOs until 2025",
+    description:
+      "Last year, investors were hopeful that 2024 would see a lot of companies going public through IPOs (Initial Public Offerings). However, as we near the middle of 2024, those hopes have mostly faded.",
     images: [
       {
         url: "https://www.mosbdigital.com/_next/image?url=https%3A%2F%2Fblog.mosbdigital.com%2F2024%2F05%2F25%2Ffrom-plaid-to-figma-heres-why-many-tech-startups-are-postponing-their-ipos-until-2025%2F53200113510_bb815ce7d9_k-jpg%2F&w=3840&q=75",
