8fb45c80fc9729b30e3906c89a2444c76fa9c0ad
mosb-digital <169149728+mosb-digital@users.noreply.github.com>
2024-05-28 13:12:50 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,46 @@
-"use client"
+import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReactQueryClientProvider } from "./ReactQueryClientProvider";
 import { Toaster } from "react-hot-toast";
-import { useMetadata } from "./hooks/useMetaData";
 
 const inter = Inter({ subsets: ["latin"] });
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  const metadata = useMetadata();
 
+// Define a default metadata object without blogPost information
+export const metadata: Metadata = {
+  title:
+    "From Plaid to Figma, here’s why many tech startups are postponing their IPOs until 2025",
+  description:
+    "Last year, investors were hopeful that 2024 would see a lot of companies going public through IPOs (Initial Public Offerings). However, as we near the middle of 2024, those hopes have mostly faded.",
+  openGraph: {
+    title: "Default Title",
+    description: "Default Description",
+    images: [
+      {
+        url: "https://www.mosbdigital.com/_next/image?url=https%3A%2F%2Fblog.mosbdigital.com%2F2024%2F05%2F25%2Ffrom-plaid-to-figma-heres-why-many-tech-startups-are-postponing-their-ipos-until-2025%2F53200113510_bb815ce7d9_k-jpg%2F&w=3840&q=75",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "article",
+  },
+  verification: {
+    google: "google",
+    yandex: "yandex",
+    yahoo: "yahoo",
+    other: {
+      me: [
+        "info@mosbdigital.com",
+        "https://www.mosbdigital.com/",
+        "mosbdigital",
+        "mosbdigital.com",
+        "mosbdigital/blog",
+      ],
+    },
+  },
+};
+
+export const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ReactQueryClientProvider>
       <html lang="en">
@@ -17,5 +50,3 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
     </ReactQueryClientProvider>
   );
 };
-
-export default RootLayout;
