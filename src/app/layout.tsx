2e87606e35fdd94633e50638199a222acabde491
Kayode <kayodefredrickbalogun@gmail.com>
2024-05-28 10:58:02 +0100

    meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,30 +6,66 @@ import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Mosb Digital",
-  description: "Fuelling Entrepreneurial Successs...",
-  verification: {
-    google: "google",
-    yandex: "yandex",
-    yahoo: "yahoo",
-    other: {
-      me: ["info@mosbdigital.com", "https://www.mosbdigital.com/", "mosbdigital", "mosbdigital.com", "mosbdigital/blog"],
-
-    },
-  },
-};
-
 export default function RootLayout({
   children,
+  metadata,
 }: {
   children: React.ReactNode;
+  metadata: Metadata;
 }) {
   return (
     <ReactQueryClientProvider>
       <html lang="en">
-        <Toaster />
-        <body className={inter.className}>{children}</body>
+        <head>
+          <title>{metadata.title as any}</title>
+          <meta
+            name="description"
+            content={metadata.description || "Default Description"}
+          />
+          {metadata.openGraph && (
+            <>
+              <meta
+                property="og:title"
+                content={metadata.openGraph.title as any}
+              />
+              <meta
+                property="og:description"
+                content={
+                  metadata.openGraph.description || "Default Description"
+                }
+              />
+              <meta
+                property="og:image"
+                content={metadata.openGraph.images?.[0]?.url}
+              />
+            </>
+          )}
+          {metadata.twitter && (
+            <>
+              <meta
+                name="twitter:card"
+                content={metadata.twitter.cardType as any}
+              />
+              <meta
+                name="twitter:title"
+                content={metadata.twitter.title as any}
+              />
+              <meta
+                name="twitter:description"
+                content={metadata.twitter.description || "Default Description"}
+              />
+              <meta
+                name="twitter:image"
+                content={metadata.twitter.images?.[0]?.url as any}
+              />
+            </>
+          )}
+          {/* Add more metadata tags as needed */}
+        </head>
+        <body className={inter.className}>
+          <Toaster />
+          {children}
+        </body>
       </html>
     </ReactQueryClientProvider>
   );
