0fb20fe00d61308e0739be209c9543c65a17e36f
mosb-digital <169149728+mosb-digital@users.noreply.github.com>
2024-05-31 16:20:22 +0100

    Update CategoryCard.tsx

--- src/components/blogs/CategoryCard.tsx
+++ src/components/blogs/CategoryCard.tsx
@@ -45,7 +45,7 @@ const CategoryCard = ({
         <TruncatedText
           // text="     Ford cuts prices on electric Mustang as demand softens for premium EVs"
           text={title}
-          maxLength={135}
+          maxLength={175}
           className="justify-start text-left mt-2 text-18 font-bold text-wrap"
           style={{ lineHeight: 1.1 }}
         />
